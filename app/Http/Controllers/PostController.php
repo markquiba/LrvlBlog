d2414238b555664195271e9b660f61194701c49e
markquiba <markjquiba@gmail.com>
2017-01-03 16:35:40 +0800

    fixed validation upon update

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -92,6 +92,7 @@ class PostController extends Controller
     public function edit($id)
     {
         $post = Post::findOrFail($id);
+
         return view('posts.edit', compact('post'));
     }
 
@@ -105,6 +106,11 @@ class PostController extends Controller
     public function update(Request $request, $id)
     {
         $post = Post::findOrFail($id);
+
+        $this->validate($request, array(
+            'title' => 'required|max:255',
+            'body' => 'required'
+        ));
 
         $post->update($request->all());
 
