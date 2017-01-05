be4c6afefe1d56f9dde6465c3877d4a017cedcf1
markquiba <markjquiba@gmail.com>
2017-01-05 08:02:41 +0800

    added contents table and seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+
         // $this->call(UsersTableSeeder::class);
+        DB::table('contents')->truncate();
+
+        DB::table('contents')->insert([
+            [
+                'meta_key' => 'title',
+                'meta_value' => 'chop chop law',
+            ],
+            [
+                'meta_key' => 'twitter',
+                'meta_value' => 'default',
+            ],
+            [
+                'meta_key' => 'github',
+                'meta_value' => 'default',
+            ],
+            [
+                'meta_key' => 'linkedin',
+                'meta_value' => 'default',
+            ]
+        ]);
     }
 }
