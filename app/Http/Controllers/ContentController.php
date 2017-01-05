166e23cbeb87187111c9512777e4ada83975dee3
markquiba <markjquiba@gmail.com>
2017-01-05 08:04:10 +0800

    added ContentController

---
+++ app/Http/Controllers/ContentController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Content;
+use Illuminate\Http\Request;
+
+class ContentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $contents = Content::all();
+        
+        return view('home', compact('contents'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $content = Content::findOrFail($id);
+
+        return view('contents.edit', compact('content'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $content = Content::findOrFail($id);
+
+        $this->validate($request, array(
+            'meta_value' => 'required'
+        ));
+
+        $content->update($request->all());
+
+        return redirect('home');
+    }
+
+}
