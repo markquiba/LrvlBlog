cd487829ecd7b28d89bc15f43510a5091b15d457
markquiba <markjquiba@gmail.com>
2017-01-05 08:05:49 +0800

    modified backend form for contents

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,9 +6,26 @@
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    You are logged in!
+                    <div class="panel-body">
+                        You are logged in!
+                        <a href="/"><button class="btn btn-lg btn-primary pull-right">View</button></a>
+                    </div>
+                    <div class="panel-body">
+                        <table class="table table-hover table-condensed">
+                            <tr>
+                                <th>Name</th>
+                                <th>Value</th> 
+                                <th></th>
+                            </tr>
+                            @foreach($contents as $content)
+                            <tr>
+                                <td>{{ $content->meta_key }}</td>
+                                <td>{{ $content->meta_value }}</td> 
+                                <td><a href="{{ action('ContentController@edit', [$content->id]) }}"><button class="btn btn-success">Edit</button></a></td>
+                            </tr>
+                            @endforeach
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
